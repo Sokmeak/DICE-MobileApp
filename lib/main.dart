1fa71db0d4bd4bcb6490ae038b0917e87470a7f4
KIMSRENG HAI <148733382+Kimsreng28@users.noreply.github.com>
2025-06-03 14:00:47 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       title: 'DICE APP',
       theme: ThemeData(
